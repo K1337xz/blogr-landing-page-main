1715145ae3c96734a2b30324ac5c9aab0261d79e
Koporson <108631492+K1337xz@users.noreply.github.com>
2022-09-15 00:10:04 +0200

    update

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const connectDropdown = document.querySelector(".connectDropdown");
 
 for (let i = 0; i < navItems.length; i++) {
 	navItems[i].addEventListener("click", () => {
+		if (navItems[i].classList.contains("trigger")) return;
+		for (let j = 0; j < navItems.length; j++) {
+			navItems[j].classList.remove("trigger");
+		}
 		navItems[i].classList.add("trigger");
 	});
 }
