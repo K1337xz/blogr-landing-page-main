43a2073106e6cb00a0edb6f000b97750c669315b
Koporson <108631492+K1337xz@users.noreply.github.com>
2022-09-14 02:28:30 +0200

    Small update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const bla = document.querySelectorAll(".mobUl > li");
+const navItems = document.querySelectorAll(".mainUl > li");
+const productDropdown = document.querySelector(".productDropdown");
+const companyDropdown = document.querySelector(".companyDropdown");
+const connectDropdown = document.querySelector(".connectDropdown");
+
+for (let i = 0; i < navItems.length; i++) {
+	navItems[i].addEventListener("click", () => {
+		navItems[i].classList.add("trigger");
+	});
+}
